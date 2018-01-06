958d0f28287689e4cbf48572147e7bd87b25220d
Red Boumghar <re.bmgr@gmail.com>
2018-01-06 20:41:36 +0000

    setup: empty setup.py for architecture
    So it starts to look like a python package.
    Lightly... :)

--- tests/test_connection.py
+++ tests/test_connection.py
@@ -4,7 +4,6 @@ import sys
 sys.path.insert(
     0,
     os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
 import dataplug
 
 def test_connection():
--- tests/test_node.py
+++ tests/test_node.py
@@ -1,4 +1,4 @@
-import pytest
+# import pytest
 import os
 import sys
 sys.path.insert(
